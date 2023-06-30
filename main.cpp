9d587facfc4fad067c332e8454b884a57d706e7f
Vera_20_17 <uts.b-11@yandex.ru>
2023-06-30 11:38:13 +0300

    Добавлены main and CMakeLists

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using std::cout;
+using std::pair;
+using std::map;
+
+
+
+int main(int argc, char const *argv[])
+{
+   
+
+    return 0;
+}
