c040cc2f0003fb26a5872730c0e282de71dd89a1
Vera_20_17 <uts.b-11@yandex.ru>
2023-07-13 19:21:21 +0300

    Add custom allocator and list

--- custom_list.cpp
+++ custom_list.cpp
@@ -1,29 +1,87 @@
+#include <iostream>
 #include <string>
-#include <algorithm>
 
-// Класс контейнера map
-template <class T, class Allocator = std::allocator<T> >
-class custom_list {
-void push_back(const T& x)
-{
-    if (size == capacity)
-    {
-        capacity = capacity * 2 + 1;
-        //T* newData = ::operator new(capacity * sizeof(T));
-        T* newData = alloc.allocate(capacity);
-        std::copy(data, data + capacity * sizeof(T), newData); //naive
-        std::swap(newData, data);
-        alloc.deallocate(newData, capacity);
+template <typename T, typename Alloc = std::allocator<T>> class MyList {
+
+public:
+  struct Node {
+    T data;
+    Node *next;
+  };
+
+  MyList(Alloc allocator = Alloc());
+  ~MyList();
+
+  void push_back(const T &data);
+
+  template <typename U, typename B>
+  friend std::ostream &operator<<(std::ostream &stream,
+                                  const MyList<U, B> &list);
+
+  class Iterator {
+  public:
+    Iterator(MyList *list) : parent(list), ptr(list->head_) {}
+    void operator++() {
+      if (ptr != nullptr)
+        ptr = ptr->next;
     }
+    typename MyList::Node &operator*() { return *ptr; }
+    typename MyList::Node *operator->() { return ptr; }
+    bool IsEnd() const { return ptr == nullptr; }
 
-    alloc.construct(data + size * sizeof(T), x);
-    ++size;
-}
+  private:
+    MyList *parent;
+    typename MyList::Node *ptr;
+  };
+
+  friend class Iterator;
 
 private:
-    std::size_t size = 0;
-    std::size_t capacity = 0;
-    T* data = nullptr;
+  void Destroy(Node *node);
 
-    Allocator alloc;
+  Node *head_;
+  Node *tail;
+
+  Alloc allocData_;
+
+  using AllocatorTraits = std::allocator_traits<Alloc>;
+  using RebindAllocNode = typename AllocatorTraits::template rebind_alloc<Node>;
+  RebindAllocNode allocNode_;
 };
+
+template <typename T, typename A>
+MyList<T, A>::MyList(A allocator)
+    : head_(nullptr), tail(nullptr), allocData_(allocator) {}
+
+template <typename T, typename A> MyList<T, A>::~MyList() {
+  auto ptr = head_;
+  while (ptr != nullptr) {
+    auto next = ptr->next;
+    Destroy(ptr);
+    ptr = next;
+  }
+}
+
+template <typename T, typename A> void MyList<T, A>::Destroy(Node *node) {
+  allocData_.destroy(node);
+  allocNode_.deallocate(node, 1);
+}
+
+template <typename T, typename A> void MyList<T, A>::push_back(const T &data) {
+  Node *node = allocNode_.allocate(1);
+  allocData_.construct(node, Node{data, nullptr});
+  if (tail != nullptr)
+    tail->next = node;
+  tail = node;
+  if (head_ == nullptr)
+    head_ = node;
+}
+
+template <typename T, typename A>
+std::ostream &operator<<(std::ostream &stream, const MyList<T, A> &list) {
+  for (typename MyList<T, A>::Node *ptr = list.head_; ptr != nullptr;
+       ptr = ptr->next)
+    stream << ptr->data << std::endl;
+
+  return stream;
+}
--- main.cpp
+++ main.cpp
@@ -2,8 +2,6 @@
 #include <map>
 #include <memory>
 #include <string>
-#include <vector>
-#include <algorithm>
 #include "custom_allocator.cpp"
 #include "custom_list.cpp"
 #include "factorial_fun.cpp"
@@ -11,17 +9,12 @@
 using std::allocator;
 using std::map;
 
-template <typename T> void inputMap(int numberTask, const T &mapInput) {
+template <typename T> void inputMap(const T &mapInput) {
 
-  std::cout << " Вывод map из пункта задания " << numberTask << " размером "
-            << mapInput.size();
-
-  std::map<int, int>::const_iterator it = mapInput.begin();
-  while (it != mapInput.end()) {
-    std::cout << " Ключ " << it->first << " - значение " << it->second
-              << std::endl;
-     it++;
-  }
+    for (const auto &pair : mapInput)
+    {
+        std::cout << pair.first << " " << pair.second << std::endl;
+    }
 }
 
 int main(int argc, char const *argv[])
@@ -36,43 +29,47 @@ int main(int argc, char const *argv[])
         - вывод на экран всех значений (ключ и значение разделены пробелом) хранящихся в контейнере
         3 - создание экземпляра своего контейнера для хранения значений типа int
         - заполнение 10 элементами от 0 до 9
-        - создание экземпляра своего контейнера для хранения значений типа int с новым аллокатором,
+        4 - создание экземпляра своего контейнера для хранения значений типа int с новым аллокатором,
         ограниченным 10 элементами
         - заполнение 10 элементами от 0 до 9
         - вывод на экран всех значений, хранящихся в контейнере 
     */
 
-// создание экземпляра std::map<int, int> - заполнение 10 элементами, где ключ - это число от 0 до 9, а значение - факториал ключа
+    // создание экземпляра std::map<int, int> - заполнение 10 элементами, где ключ - это число от 0 до 9, а значение - факториал ключа
 
     auto mapWhithStlAl  = std::map<int, int, std::less<int>, std::allocator<int>>{};
     for(size_t i = 0; i < 9; ++i)
     {
         mapWhithStlAl[i] = factorialNumber(i);
     }
-    inputMap<std::map<int, int, std::less<int>, allocator<int>>>(1, mapWhithStlAl);
+    inputMap<std::map<int, int, std::less<int>, allocator<int>>>(mapWhithStlAl);
 
     //    создание экземпляра std::map<int, int> с новым аллокатором,
     //    ограниченным 10  элементами
     auto mapWhithCustomAl = std::map<int, int, std::less<int>,
-                      custom_allocator<std::pair<const int, int>>>{};
+                      CustomAllocator<std::pair<const int, int>>>{};
     
     for(size_t i = 0; i < 9; ++i)
     {
         mapWhithCustomAl[i] = factorialNumber(i);
     }
     inputMap<map<int, int, std::less<int>,
-                 custom_allocator<std::pair<const int, int>>>>(
-        2, mapWhithCustomAl);
+                 CustomAllocator<std::pair<const int, int>>>>(mapWhithCustomAl);
 
-    /*auto v = std::vector<int, custom_allocator<int>>{};
-    for (int i = 0; i < 6; ++i) {
-        std::cout << "vector size = " << v.size() << std::endl;
-        v.push_back(i);
-		std::cout << std::endl;
+    // создание экземпляра своего контейнера для хранения значений типа int
+    //    - заполнение 10 элементами от 0 до 9
+    auto list = MyList<int, CustomAllocator<int>>{};
+
+    for (int i = 0; i < 9; ++i){
+        list.push_back(i);
     }
-*/
-    auto cList = custom_list<int, custom_allocator<int()>>{};
 
+    MyList<int, CustomAllocator<int>>::Iterator it(&list);
+    while (!it.IsEnd())
+    {
+        std::cout << it->data;
+        ++it;
+    }
 
     return 0;
 }
